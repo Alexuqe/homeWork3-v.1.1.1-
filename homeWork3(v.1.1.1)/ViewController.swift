45b229ad29639433780bdbcf2d3f213f6fb2b519
alex <hordiah1@google.com>
2024-09-01 18:39:51 +0300

    Initial Commit

---
+++ homeWork3(v.1.1.1)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homeWork3(v.1.1.1)
+//
+//  Created by Sasha on 1.09.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
